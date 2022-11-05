5de4227f9538c9e6f0fcdf519b8b06e6472366b9
nguyenkkent <nguyen.k.kent@gmail.com>
2022-11-05 13:54:28 -0700

    added functions for include.h

--- Assignment-04-Code/Include.h
+++ Assignment-04-Code/Include.h
@@ -1,1 +1,20 @@
-//	Include.h
+#pragma once
+#include "Node.cpp"
+#include "BagInterface.h"
+
+class LinkedBag : public BagInterface<ItemType> {
+
+	/*--------------------------CSC340-------------------------------------*/
+public:
+	bool removeSecondNode340();
+	bool addEnd340(const ItemType&);
+	int getCurrentSize340Iterative() const;
+	int getCurrentSize340Recursive() const;
+	int getCurrentSize340RecursiveNoHelper() const;
+	int getFrequencyOf340Recursive(const ItemType&) const;
+	int getFrequencyOf340RecursiveNoHelper(const ItemType&) const;
+	ItemType removeRandom340();
+private:
+	int getCurrentSize340RecursiveHelper(Node<ItemType>*) const; // if needed
+	int getFrequencyOf340RecursiveHelper(Node<ItemType>*, const ItemType&) const; // if needed
+	/*----------------------------------------------------------------------*/
