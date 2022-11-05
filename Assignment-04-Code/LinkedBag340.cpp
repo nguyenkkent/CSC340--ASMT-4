2e0b69c89caf6e4a3bcebf9106c0b5f91715bb34
nguyenkkent <nguyen.k.kent@gmail.com>
2022-11-05 16:01:37 -0700

    Update LinkedBag340.cpp
    Add test returns to methods for LinkedBag340

--- Assignment-04-Code/LinkedBag340.cpp
+++ Assignment-04-Code/LinkedBag340.cpp
@@ -1,8 +1,56 @@
 //	LinkedBag340.cpp
-//	Created by: CSC340
+//	Created by: Kent Nguyen
+#include "Include.h"
+using namespace std;
+#include <cstdlib>
 
-/*
-===>					<===
-===>  PLEASE ADD CODE	<===
-===>					<===
-*/
+
+template<typename ItemType>
+bool LinkedBag<ItemType>::removeSecondNode340() {
+	return true;
+}
+
+template<typename ItemType>
+bool LinkedBag<ItemType>::addEnd340(const ItemType& item) {
+	return true;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340Iterative() const {
+	return 10;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340Recursive() const {
+	return 20;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340RecursiveNoHelper() const {
+	return 30;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340Recursive(const ItemType& item) const {
+	return 40;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveNoHelper(const ItemType&item) const {
+	return 50;
+}
+
+template<typename ItemType>
+ItemType LinkedBag<ItemType>::removeRandom340() {
+	return ItemType;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(Node<ItemType>*node) const {
+	return 60;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveHelper(Node<ItemType>* node, const ItemType& item) const {
+	return 70;
+}
