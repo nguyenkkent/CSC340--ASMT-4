3d9ab137f56ded3ab763223657c625d1833b981d
nguyenkkent <frostmourne1360@yahoo.com>
2022-11-10 20:14:34 -0800

    Included number 1 and 2 of Part B

--- Part B Code/Main.cpp
+++ Part B Code/Main.cpp
@@ -2,26 +2,49 @@
 using namespace std;
 #include <string>
 #include <iostream>
+#include <memory>
+
+class MyClass {
+public:
+	MyClass() {
+		cout << "Object created";
+	}
+	~MyClass() {
+		cout << "Object destroyed";
+	}
+};
+
 
 void partBNumberOne() {
-	string str = "Fun test";
-	string* ptr1 = &str;
-	string* ptr2 = &str;
+	string* ptr1 = new string("Fun test");
+	string* ptr2 = ptr1;
 
 	cout << "Checking if both pointers point to the same memory address:" << endl
+		<< "=========================test 1============================="
 		<< "mem address that ptr1 holds: " << ptr1 << endl
-		<< "mem address that prt2 holds: " << ptr2 << endl;
+		<< "mem address that prt2 holds: " << ptr2 << endl
+		<< "============================================================" << endl;
 
 	delete ptr1;
 	ptr1 = nullptr;
 	//delete ptr2; //crashes with error
-	//ptr2 = nullptr; //
+	//ptr2 = nullptr; //crashes with error
 
 
 }
 
+void partBNumberTwo() {
+	cout << "Checking to see " << endl
+	
+}
+
+
 
 int main() {
 	partBNumberOne();
+
+	partBNumberTwo();
+
+
 
 }
