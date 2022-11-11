ec7fe792d5dfa4403f32449d51a1e37ae0866a9b
nguyenkkent <frostmourne1360@yahoo.com>
2022-11-11 15:23:18 -0800

    Completed Q4 for Part B

--- Part B Code/PartB Q4.cpp
+++ Part B Code/PartB Q4.cpp
@@ -17,3 +17,15 @@ public:
 	}
 
 };
+
+int main() {
+	unique_ptr<MyClass> uniquePtr4{ make_unique<MyClass>(25) };
+	//unique_ptr<MyClass> uniquePtr5 = uniquePtr4;//cannot make a second pointer sharing the same ownership of object
+	shared_ptr<MyClass> sharedPtr6 = move(uniquePtr4);
+	cout << "address of uniquePtr4: " << &uniquePtr4 << endl
+		 << "value that uniquePtr4 holds: " << uniquePtr4.get()<< endl
+		 << "address of sharedPtr6: " << &sharedPtr6 << endl
+		 << "value that sharedPtr6 holds: " << sharedPtr6->getData() << endl;
+
+	return 0;
+}
