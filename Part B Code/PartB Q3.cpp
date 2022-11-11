37627ee2d7d7ebc2d05eb4610ed479b9f350caa1
nguyenkkent <frostmourne1360@yahoo.com>
2022-11-11 11:58:20 -0800

    Update PartB Q3.cpp

--- Part B Code/PartB Q3.cpp
+++ Part B Code/PartB Q3.cpp
@@ -18,17 +18,31 @@ public:
 	
 };
 
-
-int main() {
-	
+void partBNumberThree() {
 	unique_ptr<MyClass> uniquePtr = make_unique<MyClass>(5);
-	cout<< "uniquePtr address:" << &uniquePtr << endl
+	cout << "uniquePtr address:" << &uniquePtr << endl
 		<< "UniquePtr .get(): " << uniquePtr.get() << endl
 		<< "UniquePtr.reset" << endl;
 	MyClass* rawPtr = { uniquePtr.release() };
 	cout << "uniquePtr address:" << &uniquePtr << endl
 		<< "UniquePtr .get(): " << uniquePtr.get() << endl
 		<< "rawPtr addres: " << &rawPtr << endl
-		<< "rawPtr owns obj @: " << rawPtr;
+		<< "rawPtr owns obj @: " << rawPtr << endl;
+}
 
+
+
+
+int main() {
+	partBNumberThree();
+
+	cout<< "Another example of if an there is no longer an ownership to an object (pointer is deleted "
+		<< "then the object dies with it's pointer" << endl;
+	{
+		unique_ptr<MyClass> uniquePtr2 = make_unique<MyClass>(10);
+		cout << "Within block uniquePtr2 value: " << uniquePtr2->getData() << endl;
+	}
+	cout << uniquePtr2.get(); //can no longer access object's data member outside of block
+
+	return 0;
 }
