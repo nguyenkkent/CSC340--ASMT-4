afeaca16ef39332710f7fd2644522f8f829f5b37
nguyenkkent <frostmourne1360@yahoo.com>
2022-11-11 17:29:01 -0800

    Complete Q5 for Part B

--- Part B Code/PartB Q5.cpp
+++ Part B Code/PartB Q5.cpp
@@ -16,11 +16,29 @@ public:
 		return value;
 	}
 
-	int main() {
-		shared_ptr<MyClass> sptr = make_shared<MyClass>(30);
-		//cout << sptr.use_count() << endl;
-		return 0;
-			
-}
-
 };
+
+int main() {
+	//MyClass* rawPtr1 = new MyClass(5);
+	//auto* rawPtr2 = rawPtr1;
+	//cout<< "Address of rawPtr1: " << &rawPtr1 << endl
+	//	<< "Pointee of rawPtr1: " << rawPtr1->getData() << endl
+	//	<< "Address of rawPtr2: " << &rawPtr2 << endl
+	//	<< "Pointee of rawPtr2: " << rawPtr2->getData() << endl;
+
+	//delete rawPtr1;
+	//rawPtr1 = nullptr;
+	//cout << "after rawPtr1 is deleted we still have rawPtr2 dangling that points to: "<< rawPtr2->getData();
+
+	shared_ptr<MyClass> sptr {new MyClass(5)};
+	weak_ptr<MyClass> wptr{ sptr };
+
+	cout << "Number of Owners for wptr: " << wptr.use_count() << endl;
+	sptr.reset();
+	cout << "Reseting shared_ptr" << endl;
+	cout << "Number of Owners for wptr: " << wptr.use_count() << endl;
+	
+
+	return 0;
+
+}
